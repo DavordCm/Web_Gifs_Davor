a5861717ee2acf62b7ad9d882a6b680efcb9b8e4
DavordCm <Jhoosman.xd@gmail.com>
2025-01-18 20:21:00 -0500

    avance de la barra de busqueda siguiente paso mejorando el estilo con material ui

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div>
-      <h1>GIF Search App</h1>
+      <h1>Busqueda de Gifs</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {gifs.map((gif) => (
           <img
